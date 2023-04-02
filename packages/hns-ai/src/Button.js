21b2751e9f015823737e84ced7da3d428564b092
Hans van Wijk <hans.v.wijk83@gmail.com>
2023-04-02 20:57:53 +0200

    -

--- packages/hns-ai/src/Button.js
+++
@@ -1,3 +0,0 @@
-Generate an accesible native Web componentButton  which has a prop for 
-style which can take the values 'primary, secondary, 
-neutral and cta', a prop for outline which can be a boolean and is set to false as default
