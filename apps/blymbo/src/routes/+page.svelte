075bebd4c3292af53012230dc9b3fdc5d99ad91b
Hans van Wijk <hans.v.wijk83@gmail.com>
2023-04-07 20:50:04 +0200

    -

--- apps/blymbo/src/routes/+page.svelte
+++ apps/blymbo/src/routes/+page.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
 	import { Stack, Container, Section, Hero, Split, Logobar } from 'hns-library';
-	import project1 from '$lib/images/project-1.webp';
-	import project2 from '$lib/images/project-2.webp';
-	import project3 from '$lib/images/project-3.webp';
-	import project4 from '$lib/images/project-4.webp';
+	import project1 from '$lib/images/project-1.png';
+	import project2 from '$lib/images/project-2.png';
+	import project3 from '$lib/images/project-3.png';
+	import project4 from '$lib/images/project-4.png';
 	import { Logo1, Logo2, Logo3, Logo4, Logo5, Logo6, Logo7, Logo8, Logo9, Logo10 } from 'logoipsum';
 	import PortfolioBlock from '$lib/components/PortfolioBlock.svelte';
 	import IconCard from '$lib/components/IconCard.svelte';
--- apps/blymbo/src/routes/portfolio/+page.svelte
+++ apps/blymbo/src/routes/portfolio/+page.svelte
@@ -2,10 +2,10 @@
 	import { Container, Section, Stack, Split, Logobar, Hero } from 'hns-library';
 	import PortfolioBlock from '$lib/components/PortfolioBlock.svelte';
 	import { Logo1, Logo2, Logo3, Logo4, Logo5, Logo6, Logo7, Logo8, Logo9, Logo10 } from 'logoipsum';
-	import project1 from '$lib/images/project-1.webp';
-	import project2 from '$lib/images/project-2.webp';
-	import project3 from '$lib/images/project-3.webp';
-	import project4 from '$lib/images/project-4.webp';
+	import project1 from '$lib/images/project-1.png';
+	import project2 from '$lib/images/project-2.png';
+	import project3 from '$lib/images/project-3.png';
+	import project4 from '$lib/images/project-4.png';
 </script>
 
 <Section>
